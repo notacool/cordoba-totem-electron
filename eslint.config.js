4ef687f50b96db6d5c1f24e357b91af80f856cb5
roman <roman.munoz@grupoapi.es>
2025-09-01 12:21:57 +0200

    chore: agrega reglas recomendadas de typescript-eslint a ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,26 @@
+
+import tseslint from 'typescript-eslint';
+
 export default [
   {
-    files: ['src/**/*.{js,jsx,ts,tsx}'],
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+        ecmaVersion: 'latest'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules
+    }
+  },
+  {
+    files: ['src/**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module'
